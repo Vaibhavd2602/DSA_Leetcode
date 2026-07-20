8a3e57c97389f81def3eb307243d2ad8025e84b6
Vaibhav Dubey <vaibhav.123456dubey@mpgi.edu.in>
2026-07-20 10:27:37 +0530

    Time: 4 ms (51.51%), Space: 15.2 MB (8.78%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+  //Most Optimal Solution
+    vector<int> twoSum(vector<int>& nums, int target) {
+         map<int, int > mp;
+         for(int i = 0 ; i < nums.size(); i++){
+            int num = nums[i];
+            int moreneeded = target - num;
+            if(mp.find(moreneeded) != mp.end()){
+                return {mp[moreneeded] , i};
+            }mp[num] = i;
+         }
+         return {-1,-1};
+    }   
+};
