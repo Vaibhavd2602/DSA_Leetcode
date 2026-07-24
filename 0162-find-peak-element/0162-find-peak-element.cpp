b9df5f85498949e6a2a38894cc7ff01429f47d16
Vaibhav Dubey <vaibhav.123456dubey@mpgi.edu.in>
2026-07-24 20:27:43 +0530

    Time: 0 ms (100%), Space: 12.7 MB (40.74%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int s = 0;
+        int end = nums.size()-1;
+        while( s < end){
+            int mid = s + (end -s)/2;
+           if(nums[mid]<nums[mid+1]) {
+            s = mid + 1;
+           }
+           else {
+            end = mid ;
+           }
+        }
+        return s;
+    }
+};
