61a591cd7073086e30850c1894e10a9994d3f004
Vaibhav Dubey <vaibhav.123456dubey@mpgi.edu.in>
2026-07-26 00:24:47 +0530

    Time: 0 ms (100%), Space: 8.7 MB (59.34%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProduct(int n) {
+        int largest = -1;
+        int secondlargest = -1;
+
+        while(n>0){
+            int digit = n%10;
+            if(digit >= largest){
+                secondlargest = largest;
+                largest = digit;
+            }
+            else if(digit > secondlargest){
+              secondlargest = digit;
+            }
+            n = n / 10;
+        }    
+        return largest*secondlargest;        
+    }
+};
