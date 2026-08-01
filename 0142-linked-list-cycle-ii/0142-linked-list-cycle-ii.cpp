811981a97b7560b89aa3a8bbb86e47abff4978e8
Vaibhav Dubey <vaibhav.123456dubey@mpgi.edu.in>
2026-08-01 19:07:19 +0530

    Time: 10 ms (45.34%), Space: 11.3 MB (55.55%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* floyddetectloop(ListNode* head) {
+        if (head == NULL) return NULL;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) return slow;
+        }
+        return NULL;
+    }
+
+    ListNode* getStartingNode(ListNode* head) {
+        if (head == NULL) return NULL;
+        ListNode* intersection = floyddetectloop(head);
+        if (intersection == NULL) return NULL;
+        ListNode* slow = head;
+        while (slow != intersection) {
+            slow = slow->next;
+            intersection = intersection->next;
+        }
+        return slow;
+    }
+    ListNode *detectCycle(ListNode *head) {
+        return getStartingNode(head);
+    }
+};
