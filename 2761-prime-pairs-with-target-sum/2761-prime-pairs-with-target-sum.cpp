ddff40e6a9f9a5e8b9ef6d9ebb11412c49804fbf
Vaibhav Dubey <vaibhav.123456dubey@mpgi.edu.in>
2026-08-03 11:26:59 +0530

    Time: 246 ms (74.9%), Space: 36.8 MB (76.85%) - LeetHub

---
+++ 2761-prime-pairs-with-target-sum/2761-prime-pairs-with-target-sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> findPrimePairs(int n) {
+        vector<bool> prime(n + 1, true);
+        vector<vector<int>> ans;
+
+        if (n < 2)
+            return ans;
+
+        prime[0] = prime[1] = false;
+
+        // Sieve of Eratosthenes
+        for (int i = 2; i * i <= n; i++) {
+            if (prime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    prime[j] = false;
+                }
+            }
+        }
+
+        // Find prime pairs
+        for (int i = 2; i <= n / 2; i++) {
+            if (prime[i] && prime[n - i]) {
+                ans.push_back({i, n - i});
+            }
+        }
+
+        return ans;
+    }
+};
