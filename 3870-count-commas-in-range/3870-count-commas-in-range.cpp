07bd5cbdf93f7b32a788efec1c6b4ecbe6555da2
Vaibhav Dubey <vaibhav.123456dubey@mpgi.edu.in>
2026-09-08 23:48:29 +0530

    Time: 0 ms (100%), Space: 8.3 MB (99.47%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,13 +1,8 @@
 class Solution {
 public:
     int countCommas(int n) {
-        int ans = 0;
-        if(n < 1000){
-            return ans;
-        }
-       for(int i = 1000 ; i <= n; i++ ){
-               ans = ans + 1;
-       }
-       return ans;        
+       int maxi = 0 ;
+       maxi = max(maxi , n-999);
+       return maxi;
     }
 };
